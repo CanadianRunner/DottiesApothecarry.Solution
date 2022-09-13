d3fa0cd97bdc37ce9dd0473ba026549e14ba0110
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-09-12 20:05:28 -0600

    Refactored for name change

--- DottiesApothecary/Controllers/HomeController.cs
+++ DottiesApothecary/Controllers/HomeController.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using DottiesApothecarry.Models;
+using DottiesApothecary.Models;
 
-namespace DottiesApothecarry.Controllers
+namespace DottiesApothecary.Controllers
 {
     public class HomeController : Controller
     {
