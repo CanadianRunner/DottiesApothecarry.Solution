b08c1e5ff11620272e253896d64b3312700b1868
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-09-12 19:43:56 -0600

    API GET AND POST COMPLETE

--- WebStoreApi/Startup.cs
+++ WebStoreApi/Startup.cs
@@ -32,10 +32,6 @@ namespace WebStoreApi
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
                 
             services.AddControllers();
-            // services.AddSwaggerGen(c =>
-            // {
-            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebStoreApi", Version = "v1" });
-            // });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,8 +40,7 @@ namespace WebStoreApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebStoreApi v1"));
+
             }
 
             //app.UseHttpsRedirection();
